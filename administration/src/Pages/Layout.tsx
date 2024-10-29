dfccddbe681f22a79df4f84b9883525d0b02aa24
Filip Studený <BluesyPompanno@seznam.cz>
2024-10-29 18:18:12 +0100

    Antd layout fix

--- administration/src/Pages/Layout.tsx
+++ administration/src/Pages/Layout.tsx
@@ -1,60 +1,151 @@
-import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
-import { useState } from "react";
-import { PageContainer, ProCard, ProLayout } from "@ant-design/pro-components";
-import { Descriptions } from "antd";
+import {
+	AppstoreOutlined,
+	BarChartOutlined,
+	CloudOutlined,
+	MenuFoldOutlined,
+	MenuUnfoldOutlined,
+	ShopOutlined,
+	TeamOutlined,
+	UploadOutlined,
+	UserOutlined,
+	VideoCameraOutlined,
+	CloseOutlined, // Import the CloseOutlined icon
+} from "@ant-design/icons";
+import { Button, Layout as PreLayout, Menu, theme, MenuProps, Drawer, Grid } from "antd";
+import React, { useState } from "react";
 
-const content = (
-	<Descriptions size='small' column={2}>
-		<Descriptions.Item label='创建人'>张三</Descriptions.Item>
-		<Descriptions.Item label='联系方式'>
-			<a>421421</a>
-		</Descriptions.Item>
-		<Descriptions.Item label='创建时间'>2017-01-10</Descriptions.Item>
-		<Descriptions.Item label='更新时间'>2017-10-10</Descriptions.Item>
-		<Descriptions.Item label='备注'>中国浙江省杭州市西湖区古翠路</Descriptions.Item>
-	</Descriptions>
-);
+const { Header, Sider, Content } = PreLayout;
+const { useBreakpoint } = Grid;
+
+const siderStyle: React.CSSProperties = {
+	overflow: "auto",
+	height: "100vh",
+	position: "fixed",
+	insetInlineStart: 0,
+	top: 0,
+	bottom: 0,
+	scrollbarWidth: "thin",
+	scrollbarGutter: "stable",
+};
+
+const items: MenuProps["items"] = [
+	UserOutlined,
+	VideoCameraOutlined,
+	UploadOutlined,
+	BarChartOutlined,
+	CloudOutlined,
+	AppstoreOutlined,
+	TeamOutlined,
+	ShopOutlined,
+].map((icon, index) => ({
+	key: String(index + 1),
+	icon: React.createElement(icon),
+	label: `nav ${index + 1}`,
+}));
 
 const Layout = () => {
 	const [collapsed, setCollapsed] = useState(false);
+	const [drawerVisible, setDrawerVisible] = useState(false);
+	const screens = useBreakpoint();
 
-	const toggleCollapsed = () => {
-		setCollapsed(!collapsed);
+	const {
+		token: { colorBgContainer, borderRadiusLG },
+	} = theme.useToken();
+
+	const isMobile = !screens.md;
+
+	const toggleDrawer = () => {
+		setDrawerVisible(!drawerVisible);
 	};
+
 	return (
-		<ProLayout
-			menuDataRender={() => [
-				{
-					path: "/item1",
-					name: "Nav Item 1",
-				},
-				{
-					path: "/item2",
-					name: "Nav Item 2",
-				},
-				{
-					path: "/item3",
-					name: "Nav Item 3",
-				},
-			]}
-		>
-			<PageContainer fixedHeader content={content}>
-				<ProCard
-					direction='column'
-					ghost
-					gutter={[0, 16]}
+		<>
+			{isMobile && (
+				<>
+					<Button
+						type='primary'
+						onClick={toggleDrawer}
+						style={{
+							position: "fixed",
+							top: 16,
+							left: 16,
+							zIndex: 1000,
+						}}
+					>
+						{drawerVisible ? <MenuFoldOutlined /> : <MenuUnfoldOutlined />}
+					</Button>
+					<Drawer
+						placement='left'
+						closable
+						onClose={toggleDrawer}
+						open={drawerVisible}
+						bodyStyle={{ padding: 0 }}
+						getContainer={false}
+						style={{ position: "fixed" }}
+						// Set the header style to match the dark theme
+						headerStyle={{ backgroundColor: "#001529", color: "white" }}
+						// Customize the close icon color
+						closeIcon={<CloseOutlined style={{ color: "white" }} />}
+					>
+						<Menu
+							theme='dark'
+							mode='inline'
+							defaultSelectedKeys={["1"]}
+							items={items}
+							style={{ width: "100%", height: "100%", paddingTop: "10px" }}
+						/>
+					</Drawer>
+				</>
+			)}
+			<PreLayout style={{ minHeight: "100vh" }}>
+				{!isMobile && (
+					<Sider collapsible collapsed={collapsed} onCollapse={setCollapsed} style={siderStyle} width={200}>
+						<Menu theme='dark' mode='inline' defaultSelectedKeys={["1"]} items={items} />
+					</Sider>
+				)}
+				<PreLayout
 					style={{
-						height: "200vh",
+						marginLeft: isMobile ? 0 : collapsed ? 80 : 200,
+						transition: "margin 0.2s",
 					}}
 				>
-					<ProCard style={{ height: 200 }} />
-					<ProCard gutter={16} ghost style={{ height: 200 }}>
-						<ProCard colSpan={16} />
-						<ProCard colSpan={8} />
-					</ProCard>
-				</ProCard>
-			</PageContainer>
-		</ProLayout>
+					<Header style={{ padding: 0, background: colorBgContainer }}>
+						{!isMobile && (
+							<Button
+								type='text'
+								icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+								onClick={() => setCollapsed(!collapsed)}
+								style={{
+									fontSize: "16px",
+									width: 64,
+									height: 64,
+								}}
+							/>
+						)}
+					</Header>
+					<Content
+						style={{
+							margin: "24px 16px",
+							padding: 24,
+							minHeight: 280,
+							background: colorBgContainer,
+							borderRadius: borderRadiusLG,
+						}}
+					>
+						Content
+						{
+							// indicates very long content
+							Array.from({ length: 100 }, (_, index) => (
+								<React.Fragment key={index}>
+									{index % 20 === 0 && index ? "more" : "..."}
+									<br />
+								</React.Fragment>
+							))
+						}
+					</Content>
+				</PreLayout>
+			</PreLayout>
+		</>
 	);
 };
 
--- administration/src/Pages/router.tsx
+++ administration/src/Pages/router.tsx
@@ -2,39 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "./Layout";
 import SignIn from "./SignIn/SignIn.page";
-
-export const AppRoutes = {
-	SignIn: {
-		path: "/sign-in",
-	},
-	DashBoard: {
-		path: "/dashboard",
-		Admin: {
-			AdminProfile: "/dashboard/profile/:id",
-		},
-		Users: {
-			path: "/dashboard/users",
-			UserDetail: {
-				path: "/dashboard/users/user/:id",
-				UserPostDetail: {
-					path: "/dashboard/users/user/:id/post/:postId",
-				},
-				UserCommentDetail: {
-					path: "/dashboard/users/user/:id/comment/:commentId",
-				},
-			},
-		},
-		Posts: {
-			path: "/dashboard/posts",
-			PostDetail: {
-				path: "/dashboard/posts/post/:id",
-				PostReports: {
-					path: "/dashboard/posts/post/:id/reports",
-				},
-			},
-		},
-	},
-};
+import { AppRoutes } from "../constants/appRoutes.router";
 
 export const routes = createBrowserRouter([
 	{
