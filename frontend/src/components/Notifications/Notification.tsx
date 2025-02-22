4b5bc18afc8dcbf1b702750901729005b57dc7ed
Filip Studený <BluesyPompanno@seznam.cz>
2025-02-22 16:33:54 +0100

    Notification clickable friend request redirect to user profile

--- frontend/src/components/Notifications/Notification.tsx
+++ frontend/src/components/Notifications/Notification.tsx
@@ -1,4 +1,5 @@
 
+import { RouterLink } from "@Components/navigation/routerLink";
 import {
 	Box,
 	ListItemAvatar,
@@ -23,37 +24,39 @@ interface NotificationItemProps {
 
 export const NotificationItem: React.FC<NotificationItemProps> = ({ notification, theme, isLast }) => {
 	return (
-		<Box>
-			<ListItemButton
-				sx={{
-					py: 2,
-					px: 3,
-					backgroundColor: notification.isRead ? "transparent" : alpha(theme.palette.primary.main, 0.04),
-					"&:hover": {
-						backgroundColor: alpha(theme.palette.primary.main, 0.08),
-					},
-				}}
-			>
-				<ListItemAvatar>
-					<Avatar
-						src={notification.creator.profilePictureUrl || undefined}
-						sx={{
-							backgroundColor: notification.creator.profilePictureUrl
-								? "transparent"
-								: getNotificationColor(notification.notificationType, theme),
-							color: getNotificationColor(notification.notificationType, theme),
-						}}
-					>
-						{!notification.creator.profilePictureUrl &&
-							`${notification.creator.firstName?.charAt(0) ?? ""}${
-								notification.creator.lastName?.charAt(0) ?? ""
-							}`}
-					</Avatar>
-				</ListItemAvatar>
-				<ListItemText primary={notification.message} secondary={formatTimeAgo(notification.createdAt)} />
-				<NotificationIcon type={notification.notificationType} theme={theme} />
-			</ListItemButton>
-			{!isLast && <Divider />}
-		</Box>
+		<RouterLink to={"/users/$id"} params={{ id: notification.creator.id }} style={{}}>
+			<Box>
+				<ListItemButton
+					sx={{
+						py: 2,
+						px: 3,
+						backgroundColor: notification.isRead ? "transparent" : alpha(theme.palette.primary.main, 0.04),
+						"&:hover": {
+							backgroundColor: alpha(theme.palette.primary.main, 0.08),
+						},
+					}}
+				>
+					<ListItemAvatar>
+						<Avatar
+							src={notification.creator.profilePictureUrl || undefined}
+							sx={{
+								backgroundColor: notification.creator.profilePictureUrl
+									? "transparent"
+									: getNotificationColor(notification.notificationType, theme),
+								color: getNotificationColor(notification.notificationType, theme),
+							}}
+						>
+							{!notification.creator.profilePictureUrl &&
+								`${notification.creator.firstName?.charAt(0) ?? ""}${
+									notification.creator.lastName?.charAt(0) ?? ""
+								}`}
+						</Avatar>
+					</ListItemAvatar>
+					<ListItemText primary={notification.message} secondary={formatTimeAgo(notification.createdAt)} />
+					<NotificationIcon type={notification.notificationType} theme={theme} />
+				</ListItemButton>
+				{!isLast && <Divider />}
+			</Box>
+		</RouterLink>
 	);
 };
